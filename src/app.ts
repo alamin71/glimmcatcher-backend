2f5d502918298fcdbe4c94ca84d96fd446bba8e0
opuopu <nurmdopu428@gmail.com>
2025-03-18 12:25:42 +0600

    feat

--- src/app.ts
+++ src/app.ts
@@ -15,7 +15,7 @@ app.use(
   cors({
     origin: true,
     credentials: true,
-    methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH'],
+    methods: ['GET', 'POST', 'DELETE', 'PATCH'],
   }),
 );
 // application routes
