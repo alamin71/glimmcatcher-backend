d9bfdc044a88c217ca912586fe234065a440bfd6
opuopu <nurmdopu428@gmail.com>
2025-04-27 11:50:06 +0600

    feat: added content routes

--- src/app/routes/index.ts
+++ src/app/routes/index.ts
@@ -1,5 +1,6 @@
 import { Router } from 'express';
 import { authRoutes } from '../modules/auth/auth.route';
+import { contentRoues } from '../modules/content/content.route';
 import { otpRoutes } from '../modules/otp/otp.routes';
 import { userRoutes } from '../modules/user/user.route';
 import walletRoutes from '../modules/wallet/wallet.route';
@@ -22,6 +23,10 @@ const moduleRoutes = [
   {
     path: '/wallet',
     route: walletRoutes,
+  },
+  {
+    path: '/content',
+    route: contentRoues,
   },
 ];
 moduleRoutes.forEach((route) => router.use(route.path, route.route));
