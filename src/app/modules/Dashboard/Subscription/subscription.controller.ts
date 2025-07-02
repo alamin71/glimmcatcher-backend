3509a384994eaa8b9f01431228d170b43b7b8261
Al-Amin <alamin50cse.miu@gmail.com>
2025-07-03 02:25:31 +0600

    add subscription

---
+++ src/app/modules/Dashboard/Subscription/subscription.controller.ts
@@ -0,0 +1,52 @@
+import { Request, Response } from 'express';
+import catchAsync from '../../../utils/catchAsync';
+import sendResponse from '../../../utils/sendResponse';
+import { StatusCodes } from 'http-status-codes';
+import { SubscriptionService } from './subscription.service';
+
+const createSubscription = catchAsync(async (req: Request, res: Response) => {
+  const result = await SubscriptionService.create(req.body);
+  sendResponse(res, {
+    statusCode: StatusCodes.CREATED,
+    success: true,
+    message: 'Subscription created successfully',
+    data: result,
+  });
+});
+
+const getAllSubscriptions = catchAsync(async (_req: Request, res: Response) => {
+  const result = await SubscriptionService.getAll();
+  sendResponse(res, {
+    statusCode: StatusCodes.OK,
+    success: true,
+    message: 'Subscriptions retrieved successfully',
+    data: result,
+  });
+});
+
+const updateSubscription = catchAsync(async (req: Request, res: Response) => {
+  const result = await SubscriptionService.update(req.params.id, req.body);
+  sendResponse(res, {
+    statusCode: StatusCodes.OK,
+    success: true,
+    message: 'Subscription updated successfully',
+    data: result,
+  });
+});
+
+const deleteSubscription = catchAsync(async (req: Request, res: Response) => {
+  const result = await SubscriptionService.delete(req.params.id);
+  sendResponse(res, {
+    statusCode: StatusCodes.OK,
+    success: true,
+    message: 'Subscription deleted successfully',
+    data: result,
+  });
+});
+
+export const SubscriptionController = {
+  createSubscription,
+  getAllSubscriptions,
+  updateSubscription,
+  deleteSubscription,
+};
