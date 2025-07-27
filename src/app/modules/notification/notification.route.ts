fd16f29861ccdf2867b9e3967bd19087d447d3cb
Al-Amin <alamin50cse.miu@gmail.com>
2025-07-27 06:33:25 +0600

    updated

--- src/app/modules/notification/notification.route.ts
+++ src/app/modules/notification/notification.route.ts
@@ -1,8 +1,14 @@
 import express from 'express';
 import { getNotifications } from '../notification/notification.controller';
+import auth from '../../middleware/auth';
+import { USER_ROLE } from '../user/user.constant';
 
 const router = express.Router();
 
-router.get('/notifications', getNotifications);
+router.get(
+  '/notifications',
+  getNotifications,
+  auth(USER_ROLE.user, USER_ROLE.admin, USER_ROLE.sup_admin),
+);
 
 export default router;
