efac610e3583d15d94a6585c47313412d70bf4ba
Al-Amin <alamin50cse.miu@gmail.com>
2025-10-02 03:54:13 +0600

    updated onbording image add title and description

--- src/app/modules/onboarding/onboarding.controller.ts
+++ src/app/modules/onboarding/onboarding.controller.ts
@@ -1,109 +1,11 @@
-// import { Request, Response } from 'express';
-// import catchAsync from '../../utils/catchAsync';
-// import sendResponse from '../../utils/sendResponse';
-// import { uploadToS3 } from '../../utils/fileHelper';
-// import { Onboarding } from './onboarding.model';
 
-// // POST (create new set of images)
-// const uploadOnboardingImages = catchAsync(async (req: Request, res: Response) => {
-//   const files = req.files as Express.Multer.File[];
-//   const uploadedImages = [];
-
-//   for (const file of files) {
-//     const image = await uploadToS3(file, 'onboarding/');
-//     uploadedImages.push(image); // {id, url}
-//   }
-
-//   const result = await Onboarding.create({ images: uploadedImages });
-
-//   sendResponse(res, {
-//     statusCode: 200,
-//     success: true,
-//     message: 'Onboarding images uploaded successfully',
-//     data: result,
-//   });
-// });
-
-// // update latest set of images
-// const updateOnboardingImages = catchAsync(async (req: Request, res: Response) => {
-//   const files = req.files as Express.Multer.File[];
-//   const uploadedImages = [];
-
-//   for (const file of files) {
-//     const image = await uploadToS3(file, 'onboarding/');
-//     uploadedImages.push(image); 
-//   }
-
-//   // find latest onboarding doc
-//   let latest = await Onboarding.findOne().sort({ createdAt: -1 });
-
-//   if (!latest) {
-//     // if no previous exists, create new
-//     latest = await Onboarding.create({ images: uploadedImages });
-//   } else {
-//     // update existing latest doc
-//     latest.images = uploadedImages;
-//     await latest.save();
-//   }
-
-//   sendResponse(res, {
-//     statusCode: 200,
-//     success: true,
-//     message: 'Onboarding images updated successfully',
-//     data: latest,
-//   });
-// });
-
-// // Always return the latest
-// const getOnboardingImages = catchAsync(async (req: Request, res: Response) => {
-//   const result = await Onboarding.findOne().sort({ createdAt: -1 }); 
-//   sendResponse(res, {
-//     statusCode: 200,
-//     success: true,
-//     message: 'Onboarding images retrieved successfully',
-//     data: result?.images || [],
-//   });
-// });
-
-// export const onboardingControllers = {
-//   uploadOnboardingImages,
-//   updateOnboardingImages,
-//   getOnboardingImages,
-// };
 import { Request, Response } from 'express';
 import catchAsync from '../../utils/catchAsync';
 import sendResponse from '../../utils/sendResponse';
 import { uploadToS3 } from '../../utils/fileHelper';
 import { Onboarding } from './onboarding.model';
 
-// POST (create new set of images with title, description)
-// const uploadOnboardingImages = catchAsync(async (req: Request, res: Response) => {
-//   const files = req.files as Express.Multer.File[];
-//   const { titles, descriptions } = req.body; // expecting array
 
-//   const uploadedImages = [];
-
-//   for (let i = 0; i < files.length; i++) {
-//     const file = files[i];
-//     const image = await uploadToS3(file, 'onboarding/');
-
-//     uploadedImages.push({
-//       id: image.id,
-//       url: image.url,
-//       title: Array.isArray(titles) ? titles[i] : titles,
-//       description: Array.isArray(descriptions) ? descriptions[i] : descriptions,
-//     });
-//   }
-
-//   const result = await Onboarding.create({ images: uploadedImages });
-
-//   sendResponse(res, {
-//     statusCode: 200,
-//     success: true,
-//     message: 'Onboarding images uploaded successfully',
-//     data: result,
-//   });
-// });
 const uploadOnboardingImages = catchAsync(async (req: Request, res: Response) => {
   const files = req.files as Express.Multer.File[];
   const { titles, descriptions } = req.body;
@@ -125,41 +27,6 @@ const uploadOnboardingImages = catchAsync(async (req: Request, res: Response) =>
   sendResponse(res, { statusCode: 200, success: true, message: 'Onboarding images uploaded successfully', data: result });
 });
 
-// update latest set of images
-// const updateOnboardingImages = catchAsync(async (req: Request, res: Response) => {
-//   const files = req.files as Express.Multer.File[];
-//   const { titles, descriptions } = req.body;
-
-//   const uploadedImages = [];
-
-//   for (let i = 0; i < files.length; i++) {
-//     const file = files[i];
-//     const image = await uploadToS3(file, 'onboarding/');
-
-//     uploadedImages.push({
-//       id: image.id,
-//       url: image.url,
-//       title: Array.isArray(titles) ? titles[i] : titles,
-//       description: Array.isArray(descriptions) ? descriptions[i] : descriptions,
-//     });
-//   }
-
-//   let latest = await Onboarding.findOne().sort({ createdAt: -1 });
-
-//   if (!latest) {
-//     latest = await Onboarding.create({ images: uploadedImages });
-//   } else {
-//     latest.images = uploadedImages;
-//     await latest.save();
-//   }
-
-//   sendResponse(res, {
-//     statusCode: 200,
-//     success: true,
-//     message: 'Onboarding images updated successfully',
-//     data: latest,
-//   });
-// });
 // Update 1, 2, or 3 images partially
 interface IUpdateImage {
   key: string;          // logical id: img1, img2, etc
--- src/app/modules/onboarding/onboarding.model.ts
+++ src/app/modules/onboarding/onboarding.model.ts
@@ -1,53 +1,5 @@
-// // // src/modules/onboarding/onboarding.model.ts
-// // import { Schema, model } from 'mongoose';
 
-// // interface IOnboarding {
-// //   images: { id: string; url: string }[]; // S3 URL save 
-// // }
-
-// // const onboardingSchema = new Schema<IOnboarding>(
-// //   {
-// //     images: [
-// //       {
-// //         id: { type: String, required: true },
-// //         url: { type: String, required: true },
-// //       },
-
-// //     ],
-// //   },
-// //   { timestamps: true }
-// // );
-
-// // export const Onboarding = model<IOnboarding>('Onboarding', onboardingSchema);
-// // src/modules/onboarding/onboarding.model.ts
-// import { Schema, model } from 'mongoose';
-
-// interface IOnboarding {
-//   images: { 
-//     id: string; 
-//     url: string; 
-//     title: string; 
-//     description: string; 
-//   }[]; 
-// }
-
-// const onboardingSchema = new Schema<IOnboarding>(
-//   {
-//     images: [
-//       {
-//         id: { type: String, required: true },
-//         url: { type: String, required: true },
-//         title: { type: String, required: true },
-//         description: { type: String, required: true },
-//       },
-//     ],
-//   },
-//   { timestamps: true }
-// );
-
-// export const Onboarding = model<IOnboarding>('Onboarding', onboardingSchema);
 import { Schema, model } from 'mongoose';
-
 interface IOnboardingImage {
   id: string;        // S3 file id
   url: string;       // S3 URL
